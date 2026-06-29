231c90354a68efbf3908df81fa90354c8a4aadd0
Chinmay Betageri <kbchinmay004@gmail.com>
2026-06-29 16:35:35 +0530

    Time: 3 ms (99.97%), Space: 70.6 MB (12.37%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
-    int minDays(vector<int>& nums, int m, int k) {
-        int n = nums.size();
-        int left = 1;
-        int right = 1e9;
-        
-        if((long long)m*k > n) return -1;
-        
-        while(left <= right) {
-            int mid = (left + right) / 2;
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        int n = bloomDay.size();
+
+        int maxi = INT_MIN;
+        for(int i = 0;i < n; i++)maxi = max(maxi, bloomDay[i]);
+        int l = 1, r = maxi;
+        int ans = -1;
+
+        while(l <= r){
+            int mid = l + (r-l)/2;
             int flower = 0, bouquet = 0;
-            
-            for (int i = 0; i < n; i++) {
-                if(nums[i] > mid) {
-                    flower = 0;
-                } else if (++flower >= k) {
-                    bouquet++;
-                    flower = 0;
+            for(int i = 0;i < n; i++){
+                if(bloomDay[i] <= mid){
+                    flower++;
+                    if(flower == k){
+                        bouquet++;
+                        flower = 0;
+                    }
                 }
-            
+                else flower = 0;
             }
-            if(bouquet < m){
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+
+            if(bouquet >= m){
+                ans = mid;
+                r = mid-1;
             }
+            else l = mid+1;
         }
-        return left;
+        return ans;
+
     }
 };
