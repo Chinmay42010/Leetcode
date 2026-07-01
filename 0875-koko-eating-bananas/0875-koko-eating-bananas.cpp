407bd91a7c3215f206d869ee18a09372d089dff0
Chinmay Betageri <kbchinmay004@gmail.com>
2026-07-02 00:01:15 +0530

    Time: 2 ms (96.64%), Space: 22.9 MB (79.19%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -3,27 +3,27 @@ public:
     int minEatingSpeed(vector<int>& a, int h) {
         int n = a.size();
         int mx = INT_MIN;
-    for (int i = 0; i < n; i++) {
-        mx = max(mx, a[i]);
-    }
-
-    int low = 1;
-    int high = mx;
-    int ans = mx;
-
-    while (low <= high) {
-        int mid = low + (high - low) / 2;
-        long long hours = 0;
         for (int i = 0; i < n; i++) {
-            hours += (a[i] + mid - 1) / mid;
+            mx = max(mx, a[i]);
         }
-        if (hours <= h) {
-            ans = mid;      
-            high = mid - 1; 
-        } else {
-            low = mid + 1;  
+
+        int low = 1;
+        int high = mx;
+        int ans = mx;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            long long hours = 0;
+            for (int i = 0; i < n; i++) {
+                hours += (a[i] + mid - 1) / mid;
+            }
+            if (hours <= h) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
         }
-    }
-    return ans ;
+        return ans;
     }
 };
