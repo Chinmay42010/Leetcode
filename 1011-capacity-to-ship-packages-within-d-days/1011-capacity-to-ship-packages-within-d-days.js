444a168acbef312559188e86e1c884f6994e54a9
Chinmay Betageri <kbchinmay004@gmail.com>
2026-07-03 23:05:43 +0530

    Time: 12 ms (73.13%), Space: 60.4 MB (12.61%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} weights
+ * @param {number} days
+ * @return {number}
+ */
+var shipWithinDays = function(nums, days) {
+    let n = nums.length;
+    let low = Math.max(...nums);
+    let high = 0;
+    for(var i = 0; i < n; i++) {
+        high += nums[i];
+    }
+    while(low <= high) {
+        let daysNeeded = 1;
+        let load = 0
+        let mid = Math.floor((low + high) / 2);
+        for(let j = 0; j < n; j++) {
+            if(load + nums[j] > mid) {
+                daysNeeded++;
+                load = 0
+            }
+            load = load + nums[j];
+        }
+        if(daysNeeded > days) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return low;
+};
