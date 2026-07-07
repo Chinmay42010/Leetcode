6826df75f633d373cb9eff44bc0e75f4fd98bce8
Chinmay Betageri <kbchinmay004@gmail.com>
2026-07-08 00:25:03 +0530

    Time: 1 ms (89.29%), Space: 55.9 MB (46.43%) - LeetHub

---
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var sumAndMultiply = function(n) {
+    let x = 0, sum = 0, len = 1;
+    while(n != 0) {
+        x = (Math.floor(n % 10)) * len + x;
+        if(Math.floor(n % 10 != 0)) len = len * 10;
+        sum += Math.floor(n % 10);
+        n = Math.floor(n / 10);
+    }
+    return sum * x;
+};
